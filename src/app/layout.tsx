8069437491923f836b1ededa09809d7e7733de6d
sandun salinda <sanjana@metaxglobal.net>
2025-12-22 13:43:35 +0530

    Fix: Add GTM and Meta Pixel directly to layout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import "@/styles/globals.css";
 import WhatsAppWidget from "@/components/ui/WhatsAppWidget";
-import GoogleTagManager from "@/components/analytics/GoogleTagManager";
-import MetaPixel from "@/components/analytics/MetaPixel";
 import StructuredData from "@/components/seo/StructuredData";
 import { siteConfig, googleSiteVerification } from "@/lib/seo-config";
 
@@ -87,11 +86,57 @@ export default function RootLayout({
   return (
     <html lang="en" className="overflow-x-hidden" data-scroll-behavior="smooth">
       <head>
-        <GoogleTagManager />
-        <MetaPixel />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5QRKP5FW');
+          `}
+        </Script>
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1166358882272505');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         <StructuredData />
       </head>
       <body className="antialiased overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QRKP5FW"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1166358882272505&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {children}
         <WhatsAppWidget phoneNumber="94771287374" />
       </body>
